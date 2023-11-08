d4d3128f0c64f4b007a5758b0230f3f47df1fadf
DanielSalas11 <daniel_salas11@outlook.com>
2023-11-08 09:01:33 -0700

    You can now change the background image and implemented permissions

--- app/src/main/java/salas/daniel/simpledrawingapp/MainActivity.kt
+++ app/src/main/java/salas/daniel/simpledrawingapp/MainActivity.kt
@@ -2,6 +2,7 @@ package salas.daniel.simpledrawingapp
 
 
 import android.app.Dialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,10 +10,88 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val cameraResultLauncher : ActivityResultLauncher<String> =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()){
+                    isGranted ->
+                if(isGranted){
+                    Toast.makeText(this,"Permission was granted",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this,"Permission was denied",Toast.LENGTH_SHORT).show()
+                    }
+            }
+    private val requestPermissions : ActivityResultLauncher<Array<String>> =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+                permissions ->
+                permissions.entries.forEach{
+                    val permissionName = it.key
+                    val isGranted = it.value
+                    if(isGranted){
+                        Toast.makeText(this@MainActivity,"Storage permission was granted",Toast.LENGTH_SHORT).show()
+
+                        val pickIntent= Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        openGalleryLauncher.launch(pickIntent)
+
+                    }else{
+                        if(permissionName==Manifest.permission.READ_EXTERNAL_STORAGE){
+                            Toast.makeText(this@MainActivity,"Storage permission was denied",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+    }
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if(result.resultCode == RESULT_OK && result.data!=null){
+                val imageBackGround:ImageView = findViewById(R.id.iv_background)
+
+                imageBackGround.setImageURI(result.data?.data)
+            }
+        }
+
+    private val cameraAndLocationResultLauncher : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+                permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+                if(isGranted){
+                    if(permissionName == Manifest.permission.ACCESS_FINE_LOCATION){
+                     Toast.makeText(this,"Permission is granted for fine location", Toast.LENGTH_SHORT).show()
+                    }
+                    if(permissionName == Manifest.permission.ACCESS_COARSE_LOCATION){
+                        Toast.makeText(this,"Permission was granted for coarse location", Toast.LENGTH_SHORT).show()
+                    }
+                    if(permissionName == Manifest.permission.CAMERA){
+                        Toast.makeText(this,"Permission is granted for camera", Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    if(permissionName == Manifest.permission.ACCESS_FINE_LOCATION){
+                        Toast.makeText(this,"Permission was denied for fine location", Toast.LENGTH_SHORT).show()
+                    }
+                    if(permissionName == Manifest.permission.ACCESS_COARSE_LOCATION){
+                        Toast.makeText(this,"Permission was denied for coarse location", Toast.LENGTH_SHORT).show()
+                    }
+                    if(permissionName == Manifest.permission.CAMERA){
+                        Toast.makeText(this,"Permission was denied for camera", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            }
+        }
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
@@ -26,16 +105,61 @@ class MainActivity : AppCompatActivity() {
 
         mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
         mImageButtonCurrentPaint!!.setImageDrawable(
-            ContextCompat.getDrawable(this,R.drawable.pallet_selected)
+            ContextCompat.getDrawable(this, R.drawable.pallet_selected)
         )
 
-        val ib_brush : ImageButton = findViewById(R.id.ib_brush)
-        ib_brush.setOnClickListener{
+        val ib_brush: ImageButton = findViewById(R.id.ib_brush)
+        ib_brush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
 
+        val ib_gallery: ImageButton = findViewById(R.id.ib_gallery)
+        /*ib_gallery.setOnClickListener {
+            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M &&
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+            showRationaleDialog("Permission Demo requires camera access",
+            "Camera cannot be used because Camera access was denied")
+            }else{
+                cameraResultLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }*/
+        /*ib_gallery.setOnClickListener {
+            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M &&
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                showRationaleDialog("Permission Demo requires camera access",
+                    "Camera cannot be used because Camera access was denied")
+            }else{
+                /*cameraAndLocationResultLauncher.launch(
+                    arrayOf(Manifest.permission.CAMERA,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION))*/
+                readExternalStorageLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }*/
 
+        ib_gallery.setOnClickListener {
+            requestStoragePermission()
+        }
+    }
 
+    private fun requestStoragePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)
+        ) {
+            showRationaleDialog(
+                "Kids Drawing App", "Kids Drawing App needs to Access your External Storage")
+        } else {
+            requestPermissions.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+            //TODO - Add writing external storage permision
+        }
+    }
+    private fun showRationaleDialog(title:String, message: String,) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 
     private fun showBrushSizeChooserDialog(){
